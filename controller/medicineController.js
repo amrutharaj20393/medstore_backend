b834fe5381e217be567433caca1b21d67c3449bd
amrutharaj20393 <amrutharaj20393@gmail.com>
2025-10-26 11:17:15 +0530

    design

--- controller/medicineController.js
+++ controller/medicineController.js
@@ -274,8 +274,8 @@ exports.makePaymentController = async (req, res) => {
             //details of product that is buying
             line_items: line_item,
             mode: 'payment',
-            success_url: "https://medstore-frontend-cobg.vercel.app/payment-succcess",
-            cancel_url: "https://medstore-frontend-cobg.vercel.app/payment-error"
+            success_url: "https://medstore-frontend-cprc.vercel.app/payment-succcess",
+            cancel_url: "https://medstore-frontend-cprc.vercel.app/payment-error"
             ///success_url: "http://localhost:5173/payment-succcess",
             ///cancel_url: "http://localhost:5173/payment-error"
         });
